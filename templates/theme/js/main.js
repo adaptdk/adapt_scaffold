d579c2925732a135bf25ca5feae6f506b39f0dc6
Frank hansen <frank@adapt.dk>
2014-02-10 13:45:06 +0100

    client theme

---
+++ templates/theme/js/main.js
@@ -0,0 +1,11 @@
+
+// Javascript executed on every page load
+
+(function ($) {
+  Drupal.behaviors.main = {
+  attach: function(context, settings) {
+   
+    // Add javascript here
+
+  }};
+})(jQuery);
