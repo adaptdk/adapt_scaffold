881eb3bd034d84b5abc653bd3b597472d204a8e0
lslinnet <larslinnet@gmail.com>
2014-02-19 08:15:38 +0100

    Updated local_setup.sh to use a specific ip for the mysql host, sockets do not work for vagrant based setups

--- templates/platform/local_setup.sh
+++ templates/platform/local_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mysqladmin -uroot -proot create {{ database }}
-mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON {{ database }}.* TO '{{ username }}'@'{{ hostname }}' IDENTIFIED BY '{{ password }}'"
+mysql -uroot -proot -h 127.0.0.1 -e "create database {{ database }}"
+mysql -uroot -proot -h 127.0.0.1 -e "GRANT ALL PRIVILEGES ON {{ database }}.* TO '{{ username }}'@'{{ hostname }}' IDENTIFIED BY '{{ password }}'"
 
 if pushd "htdocs/sites/default" > /dev/null; then
   ln -s local.settings.php settings.php
