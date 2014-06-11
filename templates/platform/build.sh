a583a38f43e83f5899d3cb48186aa2c124f510b7
Lars Stadel Linnet <larslinnet@gmail.com>
2014-06-11 09:33:11 +0200

    Added ignore regexes to drush situs build exec
    This update requires the latest version of Drush situs, so if your build fail make sure to update drush situs.

--- templates/platform/build.sh
+++ templates/platform/build.sh
@@ -1,1 +1,1 @@
-drush situs-build --root=htdocs --make-file=./platform.make --git-check
+drush situs-build --root=htdocs --make-file=./platform.make --git-check --git-check-ignore-regex=/global/,/contrib/,/libraries/
