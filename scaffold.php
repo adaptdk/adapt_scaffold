1f9303bc05aaa25287def11c67c1e4a6f943f963
lslinnet <larslinnet@gmail.com>
2014-09-15 16:34:48 +0200

    Added some mandatory update information

--- scaffold.php
+++ scaffold.php
@@ -7,7 +7,15 @@ use Symfony\Component\Console\Application;
 use Adapt\Command\CreateClientDirCommand;
 use Adapt\Command\CreateLocalRepoCommand;
 
-$application = new Application("Adapt scaffold", "2.0");
+/**
+ * <comment> is yellow text
+ * <info> is green
+ */
+
+$update_info = "\r\n\r\n<question>Did you remember to Update?</question>";
+$update_info .= "\r\n<comment>You can run:</comment> \"<info>cd " . dirname(__FILE__) . "; git pull</info>\" <comment>to update</comment>";
+
+$application = new Application("Adapt scaffold", "2.0{$update_info}");
 $application->add(new CreateClientDirCommand);
 $application->add(new CreateLocalRepoCommand);
 
