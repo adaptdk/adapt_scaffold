107e7b8c75298f7e789c5cc7c798393f6eb696c5
Lars Stadel Linnet <larslinnet@gmail.com>
2015-01-20 12:18:01 +0100

    Added automatic discovery of drush folder
    Use the build in drush discovery path to include the drush folder instead of the custom script.
    Discovery will work when inside the htdocs folder and can be used to centralize the inclusion of site aliases, commands and common drush settings.

--- templates/platform/build.sh
+++ templates/platform/build.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
 drush situs-build --root=htdocs --make-file=./platform.make --git-check --git-check-ignore-regex=/global/,/contrib/,/libraries/
+
+if pushd "htdocs/sites/all" > /dev/null; then
+  if [ ! -L "drush" ]; then
+    ln -s ../../../drush .
+    echo "Symlink created for drush."
+  fi
+  popd > /dev/null;
+fi
