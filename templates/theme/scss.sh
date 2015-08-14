cbdbe9ebf13a5811ff310708aa6143dcddce7ee7
Casper Juel <casper@adapt.dk>
2015-08-14 11:31:46 +0200

    unnecessary

--- templates/theme/scss.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-node-sass \
-  --include-path node_modules/node-bourbon/node_modules/bourbon/app/assets/stylesheets \
-  --include-path node_modules/node-neat/node_modules/bourbon-neat/app/assets/stylesheets \
-  -w  \
-  --source-map true \
-  sass/ -o css \
